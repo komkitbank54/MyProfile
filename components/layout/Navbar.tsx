6437715bbdee2de1689fa87761ca0793a8329c66
ikkist <komkit.n@fnsyrus.com>
2025-11-19 00:50:13 +0700

    #4 Developing Navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,9 +1,32 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export default function Navbar() {
+    const [isFixed, setIsFixed] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollThreshold = window.innerHeight * 0.17;
+            setIsFixed(window.scrollY >= scrollThreshold);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
         <div className="">
-            <div className="h-[17vh]" />
-            <nav className="sticky bg-sky-100 top-0 shadow-lg z-50">
-                <div className="flex justify-between">
+            <div className="h-[17vh] flex justify-center">
+                <div className="w-50 h-50 bg-sky-300 rounded-full flex justify-center shadow-2xl">
+                    <div className="w-48 h-48 bg-sky-400 rounded-full mt-1"></div>
+                </div>
+            </div>
+            <nav
+                className={`${
+                    isFixed ? "fixed top-0 left-0 right-0" : "relative"
+                }  bg-sky-100 shadow-lg z-50`}
+            >
+                <div className="flex justify-between text-sky-800">
                     <div>A</div>
                     <div>S</div>
                     <div>E</div>
