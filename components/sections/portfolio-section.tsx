6c79e71067272700ec5847799c98d204334a91e0
ikkist <komkit.n@fnsyrus.com>
2025-11-19 23:56:03 +0700

    g

---
+++ components/sections/portfolio-section.tsx
@@ -0,0 +1,7 @@
+export default function PortfolioSection() {
+    return (
+        <div className="border-red-500 border-2 h-[95vh]">
+            <h1>Portfolio Section</h1>
+        </div>
+    );
+}
---
+++ components/sections/skills-section.tsx
@@ -0,0 +1,7 @@
+export default function SkillsSection() {
+    return (
+        <div className="border-red-500 border-2 h-[95vh]">
+            <h1>Skills Section</h1>
+        </div>
+    );
+}
