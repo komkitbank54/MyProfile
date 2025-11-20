487c16c24edf4e54d07b78ffedc01cc51653643c
ikk <83362548+komkitbank54@users.noreply.github.com>
2025-11-21 00:11:12 +0700

    Dev (#12)
    * #5 Developing Navbar
    - Redesigned to pixelart style
    - Added About Icon
    
    * #6 Developing Navbar
    
    * #6 Developing Navbar
    
    * #7 Added Navbar Icon
    
    * g
    
    * #9  Fixed Navbar bug
    - Added snap sections
    
    ---------
    
    Co-authored-by: NB0524-4195\FSS <komkit.n@fnsyrus.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useRef } from "react";
+
+import Navbar from "@/components/layout/Navbar";
 import AboutSection from "@/components/sections/about-section";
 import ExpSection from "@/components/sections/experience-section";
 import PortfolioSection from "@/components/sections/portfolio-section";
@@ -5,18 +10,33 @@ import ResumeSection from "@/components/sections/resume-section";
 import SkillsSection from "@/components/sections/skills-section";
 
 export default function Home() {
+    const scrollRef = useRef(null);
 
     return (
-        <div className="bg-[#56a1e4] snap-y snap-mandatory h-screen overflow-y-scroll">
-            <section className="snap-start">
-                <AboutSection />
-            </section>
-            <section className="snap-start">
-                <SkillsSection />
-            </section>
-            <ExpSection />
-            <PortfolioSection />
-            <ResumeSection />
+        <div
+            ref={scrollRef}
+            className="snap-y snap-mandatory h-screen overflow-y-scroll"
+        >
+            <div className="snap-start">
+                <Navbar scrollRef={scrollRef} />
+            </div>
+            <div className="bg-[#56a1e4]">
+                <section className="">
+                    <AboutSection />
+                </section>
+                <section className="snap-start pt-[4.8vh]">
+                    <SkillsSection />
+                </section>
+                <section className="snap-start pt-[4.8vh]">
+                    <ExpSection />
+                </section>
+                <section className="snap-start pt-[4.8vh]">
+                    <PortfolioSection />
+                </section>
+                <section className="snap-start pt-[4.8vh]">
+                    <ResumeSection />
+                </section>
+            </div>
         </div>
     );
 }
